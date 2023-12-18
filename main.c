6aad6f5969cb8d366f857c03e2db89a785e94f96
9128hotel <9128hotel@gmail.com>
2023-12-18 20:15:44 +0000

    new file:   parser.c new file:   parser.h

--- main.c
+++ main.c
@@ -3,17 +3,19 @@
 #include <stdlib.h>
 
 int main() {
-    char* command = (char*)malloc(sizeof(char) * 101);
+    char* raw = (char*)malloc(sizeof(char) * 101);
 
-    if (command == NULL) {
+    if (raw == NULL) {
         fprintf(stderr, "Memory allocation failed\n");
         return 1;
     }
 
     printf("Enter sum (max 100 char): ");
-    scanf("%100s", command);
-    printf("Print: %s", command);
-    free(command);
+    scanf("%100s", raw);
+    printf("Print: %s", raw);
+
+    char* postparse = parse(raw);
+    free(raw);
 
     return 0;
 }
